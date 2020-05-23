d09de908e8abb7d82cfcb8e24cfc27ac13dc6c49
briller8 <briller015@gmail.com>
2020-05-23 23:58:50 +0900

    write HTML,CSS

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,5 +1,12 @@
 'use strict'
+    (function () {
 
-    (() => {
+        const Http = async ({ url, method, data }) => {
+
+        }
+
+        const getPosts = async () => { }
+        const getPosts = async () => { }
+        const getPosts = async () => { }
 
     })();
