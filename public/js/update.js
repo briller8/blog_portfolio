b4bf2994e40407de47cb3590ce7de3ee15f8318c
briller8 <briller015@gmail.com>
2020-05-24 20:14:00 +0900

    delete JS

--- public/js/update.js
+++ public/js/update.js
@@ -14,8 +14,7 @@
             const { author, title, content, id, tag } = result.data
 
 
-            document.querySelector("#blogTitle").inne
-            rHTML = title;
+            document.querySelector("#blogTitle").innerHTML = title;
             document.querySelector("#blogContent").innerHTML = content;
             document.querySelector("#blogName").value = author;
             document.querySelector("#blogTag").value = tag;
@@ -31,21 +30,49 @@
     ////////////////////
 
     if (delBtn) {
-        delBtn.addEventListener("click", 
+        delBtn.addEventListener("click", async (e) => {
+            e.preventDefault();
+            const password = document.querySelector("#blogPw").value;
+            if (!password.trim()) {
+                return alert("패스워드를 입력해주세요");
+            }
+            const result = await blogDel({ postId, password });
+            console.log(result);
+            if (result.status === "success") {
+                alert("삭제되었습니다");
+                window.location = "/blog.html"
+            } else {
+                alert("패드워드를 확인해주세요");
+            }
+        });
     }
 
+
+
+
+
+    ////////////////////
+    //    api call    //
+    ////////////////////
+
+    //get blogDel
     const blogDel = ({ postId, password }) => {
         return new Promise((res, rej) => {
 
             const obj = {
                 password
             }
+            fetch(`${API_HOST}/post/${postId}`, {
+                method: "DELETE",
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(obj)
+            }).then(res => res.json())
+                .then(json => res(json))
+                .catch(e => rej(e))
         })
     }
 
-
-
-
+    //get Blog
     const getBlog = ({ id }) => {
         console.log(id);
         return new Promise((res, rej) => {
