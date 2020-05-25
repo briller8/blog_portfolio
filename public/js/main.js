7c402bf56695ec5ca43ff7f27d8f672d67ce1c5f
briller8 <briller015@gmail.com>
2020-05-25 23:45:24 +0900

    Detail JS

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight:${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight:${navbarHeight}`);
 
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
@@ -13,3 +13,75 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+
+});
+
+
+//transparent as window scroll down
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    // console.log(home);
+    // console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+//Show arrowBtn
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+const arrowUp = document.querySelector('.arrow-up');
+console.log(arrowUp);
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#Home');
+});
+
+
+//My work filter
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    projects.forEach((project) => {
+        console.log(filter);
+        console.log(filter === "All");
+        // console.log(project.dataset.type);
+        if (filter === "All") {
+            project.classList.remove('invisible');
+        }
+        if (filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+});
+
